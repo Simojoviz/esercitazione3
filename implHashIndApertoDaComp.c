d9c3a17c10f22793a9db457c959d1846ec721b8c
Simojoviz <simonejov@gmail.com>
2021-03-11 11:51:05 +0100

    start es4

--- implHashIndApertoDaComp.c
+++ implHashIndApertoDaComp.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dizionario.h"
-#define HASHSIZE ...
-#define HASHAUX ...
+#define HASHSIZE 8
+#define HASHAUX -1
 
 struct elem{
   int key;
@@ -19,18 +19,43 @@ struct dizionario{
 };
 
 int funHash(int val, int i){
-  ...
+  return ((val % HASHSIZE) + (i * (1+2*(val % (HASHSIZE-3))))) % HASHSIZE;
 }
 
 /*post: restituisce un dizionario vuoto */
 Dizionario crea(){
-  ...
+  Dizionario new;
+  int i;
+
+  new = (Dizionario) malloc(sizeof(struct dizionario));
+  new->content = (Elem) malloc(sizeof(strct elem) * HASHSIZE);
+  new->numchiavi = 0;
+  new->DEL = NULL;
+
+  for (i = 0; i < HASHSIZE; i++) 
+    new->content[i] = NULL;
+
+  return new;
 }
 
 /*pre: k non e' presente in d e c'e' spazio in d*/
 /*post: associa il valore val alla chiave k nel dizionario d*/
 void inserisci(Dizionario d, int k, int val){
-  ...
+  int i = 0;
+  int j;
+  int trovato = false;
+  int newElem;
+  do{
+    j = funHash(k, i);
+    if (v->content[j] == NULL || v->content[j] == HASHAUX)
+      trovato = true;
+    else 
+      i++;
+  } while(!trovato && i < HASHSIZE);
+
+  newElem = (Elem) malloc(sizeof(struct elem));
+  newElem->key;
+
 }
 
 /*pre: l'elemento x e' contenuto nel dizionario d*/
